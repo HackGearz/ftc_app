92a55989ab97a0bf3f4fbea0317732a91a2c2b0b
smcdowell <steve.mcdowell@myfairpoint.net>
2018-10-24 18:40:18 -0400

    Add subsystem classes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain.java
@@ -2,4 +2,13 @@ package org.firstinspires.ftc.teamcode;
 
 public class Drivetrain {
 
+    // Drive in tank mode
+    // args:
+    //   leftSpeed: left side wheels speed, integer range 0 to 100
+    //   rightSpeed: right side wheels speed, integer range 0 to 100
+    public void DriveTank(int leftSpeed, int rightSpeed) {
+        // uses motor type: ????
+
+
+    }
 }
